ed17e98af7b07f12f1497ac751b2bb7affde3774
omame <me@omame.tech>
2021-02-17 13:21:32 +0100

    Remove Weblate header

--- src/Footer.js
+++ src/Footer.js
@@ -8,17 +8,9 @@ export default class Footer extends React.Component {
     let wipDisclaimerHTML = getLanguageKey("wipDisclaimer")
       .replace("[", '<a href="https://github.com/cyberos/cyberos.github.io">')
       .replace("]", "</a>");
-    let widgetColor = isDarkMode() ? "black" : "white";
     return (
       <div className="footer-container">
         <div className="footer-content">
-          <p>
-            <a href="https://hosted.weblate.org/engage/cyberos/">
-              <img
-                src={`https://hosted.weblate.org/widgets/cyberos/-/287x66-${widgetColor}.png`}
-              />
-            </a>
-          </p>
           <p className="footer-copyright-notice">
             {getLanguageKey("footerCopyright")}
           </p>
