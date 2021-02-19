cd341548f10e09121b63058b7e4bda91e6a9f2b3
omame <me@omame.tech>
2021-02-19 14:14:10 +0100

    Improve translation handler

--- src/Translations.js
+++ src/Translations.js
@@ -1,19 +1,38 @@
-import en_US from "./translations/en-US.json";
-import pl from "./translations/pl.json";
 import de from "./translations/de.json";
+import en_US from "./translations/en-US.json";
+import es_MX from "./translations/es-MX.json";
+import pl from "./translations/pl.json";
+import pt_BR from "./translations/pt-BR.json";
 import ru from "./translations/ru.json";
+import zh_Hans from "./translations/zh-Hans.json";
+import zh_Hant_HK from "./translations/zh-Hant-HK.json";
+import zh_Hant from "./translations/zh-Hant.json";
 
 let translations = {
-  en_US: en_US.website,
-  pl: pl.website,
-  de: de.website,
-  ru: ru.website,
+  de: de,
+  "en-US": en_US,
+  "es-MX": es_MX,
+  pl: pl,
+  "pt-BR": pt_BR,
+  ru: ru,
+  "zh": zh_Hans,
+  "zh-CN": zh_Hans,
+  "zh-HK": zh_Hant_HK,
+  "zh-TW": zh_Hant,
 };
 
 export function getLanguage() {
-  return navigator.language in translations ? navigator.language : "en_US";
+  return navigator.language in translations ? navigator.language : "en-US";
 }
 
 export default function getLanguageKey(key) {
-  return translations[getLanguage()][key];
+  const keys = translations[getLanguage()];
+
+  let rootKey = keys.website;
+  if (!rootKey) rootKey = en_US.website;
+
+  let output = rootKey[key];
+  if (!output) output = en_US.website[key];
+
+  return output;
 }
